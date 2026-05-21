665dc267ebe93369138d7edfb2a50be2de34e62d
Daniele Silva <daniele.silva@ifce.edu.br>
2026-05-21 17:30:06 -0300

    Ajustes de alertas + validação de usuarios e senhas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from galeria.views import index, imagem, buscar, surpreenda_me
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name='buscar'),
-    path('surpreenda-me/', surpreenda_me, name='surpreenda-me'),
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
@@ -137,4 +138,3 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
